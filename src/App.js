520c016220237e5fa693a67b4d893a67c5f95462
achouqfi <75440007+achouqfi@users.noreply.github.com>
2022-08-30 17:24:30 +0100

    react native boilerplate

---
+++ src/App.js
@@ -0,0 +1,32 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {NavigationNativeContainer} from '@react-navigation/native';
+import {ApolloProvider} from '@apollo/react-hooks';
+
+import AppNavigator from './navigation/AppNavigator';
+import ContextProvider from './tools/context';
+import client from './tools/apollo';
+import './tools/firebase';
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <ContextProvider>
+        <QueryClientProvider >
+          <NavigationNativeContainer>
+            <AppNavigator />
+          </NavigationNativeContainer>
+        </QueryClientProvider>
+      </ContextProvider>
+    </ApolloProvider>
+  );
+}
+
+export default App;
